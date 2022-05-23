f35ae924156ba4719dd263f9e0f69edbd7f9314d
GUEI <sofiabsb02@gmail.com>
2022-05-23 15:51:27 -0300

    questão 1 da lista 1

---
+++ 3° Semestre/2 - Estrutura de Dados/Lista 1/Questão 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Questão_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VERIFICADOR DE PALÍNDROMOS");
+            Console.WriteLine("Insira uma palavra");
+            string palavraAVerificar = Console.ReadLine();
+
+            if (EPalindromo(palavraAVerificar))
+            {
+                Console.WriteLine($"A palavra: {palavraAVerificar} é um palíndromo");
+            }
+            else
+            {
+                Console.WriteLine($"A palavra: {palavraAVerificar} não é um palíndromo");
+            }
+        }
+
+        static bool EPalindromo(string palavra)
+        {
+            Stack<char> pilhaDeLetras = new Stack<char>();
+            string palavraInvertida = "";
+            foreach (var letra in palavra)
+            {
+                pilhaDeLetras.Push(letra);
+            }
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                palavraInvertida += pilhaDeLetras.Pop();
+            }
+            if (palavraInvertida == palavra)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
